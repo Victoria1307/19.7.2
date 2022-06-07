20e1f0cc5f96bec58d670a34578fa822a16d4860
Viki <vandro1307@gmail.com>
2022-06-07 22:07:41 +0300

    Finished

--- api.py
+++ api.py
@@ -9,7 +9,7 @@ class Boxberry:
     def get_list_cities(self, token: str):
         """ Метод получения списка всех городов"""
         path = f'json.php?token={token}&method=ListCities'
-        url = self.base_url+path
+        url = self.base_url + path
         print(f'url: {url}')
         res = requests.get(url)
         print(f'res = {res}')
@@ -21,9 +21,24 @@ class Boxberry:
             result = res.text
         return status, result
 
-    def get_moscow_points(self, token: str, city_code: str, country_code: str):
+    def get_points(self, token: str, city_code: str, country_code: str):
         """Метод получения точек по коду города и страны"""
         path = f'json.php?token={token}&method=ListPoints&prepaid=1&CityCode={city_code}&CountryCode={country_code}'
+        url = self.base_url + path
+        print(f'url: {url}')
+        res = requests.get(url)
+        print(f'res = {res}')
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
+
+    def get_point_description(self, token: str, point_code: str):
+        """Получить полную информацию по пункту выдачи"""
+        path = f'json.php?token={token}&method=PointsDescription&code={point_code}'
         url = self.base_url + path
         print(f'url: {url}')
         res = requests.get(url)
@@ -39,7 +54,7 @@ class Boxberry:
     def get_list_zips(self, token: str):
         """ Метод получения списка ZIP кодов"""
         path = f'json.php?token={token}&method=ListZips'
-        url = self.base_url+path
+        url = self.base_url + path
         print(f'url: {url}')
         res = requests.get(url)
         print(f'res = {res}')
@@ -66,5 +81,32 @@ class Boxberry:
             result = res.text
         return status, result
 
+    def get_courier_list_cities(self, token: str):
+        """ Получить перечень городов, в которых осуществляется курьерская доставка"""
+        path = f'json.php?token={token}&method=CourierListCities'
+        url = self.base_url + path
+        print(f'url: {url}')
+        res = requests.get(url)
+        print(f'res = {res}')
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
 
-
+    def get_point_for_parcels(self, token: str):
+        """ Получить список пунктов приема посылок."""
+        path = f'json.php?token=d6f33e419c16131e5325cbd84d5d6000&method=PointsForParcels'
+        url = self.base_url + path
+        print(f'url: {url}')
+        res = requests.get(url)
+        print(f'res = {res}')
+        status = res.status_code
+        result = ""
+        try:
+            result = res.json()
+        except json.decoder.JSONDecodeError:
+            result = res.text
+        return status, result
--- tests/test_boxberry.py
+++ tests/test_boxberry.py
@@ -15,23 +15,30 @@ def test_get_list_cities(token=TOKEN):
 
 def test_get_moscow_points(token=TOKEN):
     """Проверяем получение все точек в Москве"""
-    status, result = boxberry.get_moscow_points(token, '68', '643')
+    status, result = boxberry.get_points(token, '68', '643')
     assert status == 200
     assert len(result) > 0
     assert 'Metro' in result[0]
 
 
+def test_get_point_description(token=TOKEN):
+    """Проверяем получение полной информации по пункту выдачи"""
+    status, result = boxberry.get_point_description(token, '99451')
+    assert status == 200
+    assert 'WorkShedule' in result
+
+
 def test_get_city_points_not_found(token=TOKEN):
     """Проверяем получение все точек с несуществующем кодом города """
-    status, result = boxberry.get_moscow_points(token, '10001', '643')
+    status, result = boxberry.get_points(token, '10001', '643')
     assert status == 200
     assert len(result) == 1
     assert result[0].get('err') == 'Данные не найдены'
 
 
-def test_get_city_points_not_found(token=TOKEN):
+def test_not_found_country(token=TOKEN):
     """Проверяем получение все точек с несуществующем кодом страны """
-    status, result = boxberry.get_moscow_points(token, '68', '77777')
+    status, result = boxberry.get_points(token, '68', '77777')
     assert status == 200
     assert len(result) == 1
     assert result[0].get('err') == 'Данные не найдены'
@@ -55,8 +62,26 @@ def test_check_zip(token=TOKEN):
 
 
 def test_check_unknown_zip(token=TOKEN):
-    """Проверяем метод проверки данных по определенному ZIP коду"""
+    """Проверяем метод проверки данных по несуществующему ZIP коду"""
     status, result = boxberry.check_zip(token, '12')
     assert status == 200
     assert len(result) == 1
     assert result.get('err') == 'Данные не найдены'
+
+
+def test_get_courier_list_cities(token=TOKEN):
+    """Проверяем метод получения городов, в которых осуществляется курьерская доставка."""
+    status, result = boxberry.get_courier_list_cities(token)
+    assert status == 200
+    assert len(result) > 1
+    assert 'Area' in result[0]
+    assert 'DeliveryPeriod' in result[0]
+
+
+def test_get_point_for_parcels(token=TOKEN):
+    """Проверяем метод получения списка пунктов приема посылок."""
+    status, result = boxberry.get_point_for_parcels(token)
+    assert status == 200
+    assert len(result) > 1
+    assert 'Name' in result[0]
+    assert 'City' in result[0]
